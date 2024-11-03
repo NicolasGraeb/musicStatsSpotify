2dbfd7269b4965a5e649369ae0e1b2adc7607d14
NicolasPBS <nicgra000@pbs.edu.pl>
2024-11-03 21:57:50 +0100

    dadd

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 export const clientId = '8dd0190c59e74083b9c4a1ec76943c6b';
-export const redirectUri = 'http://localhost:4173';
+export const redirectUri = 'spotify-best-artists-songs-tqvq.vercel.app';
 const scope = 'user-read-private user-read-email user-top-read';
 
 export const generateRandomString = (length) => {
